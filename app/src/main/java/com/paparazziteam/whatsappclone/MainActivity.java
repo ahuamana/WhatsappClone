036481ee1d7a5534cd625a6c8d4386e4c2cad248
Alikhan <73962071@continental.edu.pe>
2021-06-11 18:01:30 -0500

    country code picker on main activity

--- app/src/main/java/com/paparazziteam/whatsappclone/MainActivity.java
+++ app/src/main/java/com/paparazziteam/whatsappclone/MainActivity.java
@@ -6,24 +6,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
 
     Button mButtonSendCode;
+    EditText mEditTextPhone;
+    CountryCodePicker mCountryCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mButtonSendCode = findViewById(R.id.btnSendCode);
+        mEditTextPhone = findViewById(R.id.editTextPhone);
+        mCountryCode = findViewById(R.id.cpp);
+
+
         mButtonSendCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToVerificationActivity();
+                //goToVerificationActivity();
+                getData();
             }
         });
 
 
+    }
+    public void getData()
+    {
+        String code = mCountryCode.getSelectedCountryCodeWithPlus();
+        String phone = mEditTextPhone.getText().toString();
+
+        if(phone.equals(""))
+        {
+            Toast.makeText(this, "Debes ingresar un numero de telefono", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Telefono: " + code +" " + phone, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void goToVerificationActivity(){
