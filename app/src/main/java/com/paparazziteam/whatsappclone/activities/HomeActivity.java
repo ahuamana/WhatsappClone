30bb96da3d26a1a68ada54d24a348a16287f7918
Alikhan <73962071@continental.edu.pe>
2021-06-14 09:36:48 -0500

    disegn xml from home activity

--- app/src/main/java/com/paparazziteam/whatsappclone/activities/HomeActivity.java
+++ app/src/main/java/com/paparazziteam/whatsappclone/activities/HomeActivity.java
@@ -1,35 +1,49 @@
 package com.paparazziteam.whatsappclone.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.paparazziteam.whatsappclone.R;
 import com.paparazziteam.whatsappclone.providers.AuthProvider;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements MaterialSearchBar.OnSearchActionListener {
 
-    Button mButtonSignOut;
     AuthProvider mAuthProvider;
+
+    MaterialSearchBar mSearchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mButtonSignOut = findViewById(R.id.btnsignOut);
+        mSearchBar = findViewById(R.id.searchBar);
+
+        mSearchBar.setOnSearchActionListener(this); //implementar metodos para buscar
+        mSearchBar.inflateMenu(R.menu.main_menu); //Añadir el menu opciones al search bar
+        mSearchBar.getMenu().setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                if(item.getItemId() == R.id.itemSignOut)
+                {
+                    signOut();
+                }
+
+                return true;
+            }
+        });
 
         mAuthProvider = new AuthProvider();
 
-        mButtonSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signOut();
-            }
-        });
+
 
     }
 
@@ -38,5 +52,22 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(HomeActivity.this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+
+    //Methos implemented from search bar
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+
+    }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+
+    }
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
     }
 }
