39493f6aa25ef857f857bb6f0f16449f27cfca26
Alikhan <73962071@continental.edu.pe>
2021-06-15 15:03:55 -0500

    show data from username

--- app/src/main/java/com/paparazziteam/whatsappclone/activities/ProfileActivity.java
+++ app/src/main/java/com/paparazziteam/whatsappclone/activities/ProfileActivity.java
@@ -3,11 +3,27 @@ package com.paparazziteam.whatsappclone.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.paparazziteam.whatsappclone.R;
+import com.paparazziteam.whatsappclone.models.User;
+import com.paparazziteam.whatsappclone.providers.AuthProvider;
+import com.paparazziteam.whatsappclone.providers.UsersProvider;
 import com.paparazziteam.whatsappclone.utils.Mytoolbar;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    UsersProvider mUsersProvider;
+    AuthProvider mAuthProvider;
+
+    TextView mTextViewUsername;
+    TextView mTextViewPhone;
+    CircleImageView mCircleImageProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +32,41 @@ public class ProfileActivity extends AppCompatActivity {
 
         Mytoolbar.show(this,"Perfil",true); //mostrar toolbar
 
+        mUsersProvider = new UsersProvider();
+        mAuthProvider = new AuthProvider();
+
+        mTextViewUsername = findViewById(R.id.textViewUsername);
+        mTextViewPhone = findViewById(R.id.textViewPhone);
+        mCircleImageProfile = findViewById(R.id.circleImageProfile);
+        
+        getUserInfo();
+
+    }
+
+    private void getUserInfo() {
+        mUsersProvider.getUserInfo(mAuthProvider.getID()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() { // .get para obtener la informacion
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                if(documentSnapshot.exists())
+                {
+                    User user = documentSnapshot.toObject(User.class);//seteamos a nuestra clase modelo user para almacenar todos los datos ahi
+                    mTextViewUsername.setText(user.getUsername());
+                    mTextViewPhone.setText(user.getPhone());
+
+                    if(user.getImage()!= null)
+                    {
+                        if(!user.getImage().equals(""))
+                        {
+                            Glide.with(ProfileActivity.this)
+                                    .load(user.getImage())
+                                    .into(mCircleImageProfile);
+                        }
+                    }
+
+                }
+
+            }
+        });
     }
 }
