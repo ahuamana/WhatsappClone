ced1d08800ae8c62dceadaf115430bd2c53d3111
Alikhan <73962071@continental.edu.pe>
2021-06-23 06:48:28 -0500

    added model to manage message

---
+++ app/src/main/java/com/paparazziteam/whatsappclone/models/Message.java
@@ -0,0 +1,81 @@
+package com.paparazziteam.whatsappclone.models;
+
+public class Message {
+
+    private String id;
+    private String idSender;
+    private String idReceiver;
+    private String idChat;
+    private String message;
+    private String status;
+    private Long timstamp;
+
+    public Message() {
+    }
+
+    public Message(String id, String idSender, String idReceiver, String idChat, String message, String status, Long timstamp) {
+        this.id = id;
+        this.idSender = idSender;
+        this.idReceiver = idReceiver;
+        this.idChat = idChat;
+        this.message = message;
+        this.status = status;
+        this.timstamp = timstamp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdSender() {
+        return idSender;
+    }
+
+    public void setIdSender(String idSender) {
+        this.idSender = idSender;
+    }
+
+    public String getIdReceiver() {
+        return idReceiver;
+    }
+
+    public void setIdReceiver(String idReceiver) {
+        this.idReceiver = idReceiver;
+    }
+
+    public String getIdChat() {
+        return idChat;
+    }
+
+    public void setIdChat(String idChat) {
+        this.idChat = idChat;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getTimstamp() {
+        return timstamp;
+    }
+
+    public void setTimstamp(Long timstamp) {
+        this.timstamp = timstamp;
+    }
+}
