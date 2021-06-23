78c91eb8b28c1bd3321c526fece754b849940335
Alikhan <73962071@continental.edu.pe>
2021-06-23 06:52:47 -0500

    added provider to work and set data with custom objects

---
+++ app/src/main/java/com/paparazziteam/whatsappclone/providers/MessageProvider.java
@@ -0,0 +1,19 @@
+package com.paparazziteam.whatsappclone.providers;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.paparazziteam.whatsappclone.models.Message;
+
+public class MessageProvider {
+    CollectionReference mCollection;
+
+    public MessageProvider() {
+        mCollection = FirebaseFirestore.getInstance().collection("Message");
+    }
+
+    public Task<Void> create (Message message)
+    {
+        return mCollection.document().set(message);
+    }
+}
