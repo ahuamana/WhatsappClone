90b2708cd79fbbec31d7422293bae06bda9b66ab
Alikhan <73962071@continental.edu.pe>
2021-07-21 09:45:12 -0500

    implemented cloud messaging for recieve notifications

---
+++ app/src/main/java/com/paparazziteam/whatsappclone/services/MyFirebaseMessagingClient.java
@@ -0,0 +1,22 @@
+package com.paparazziteam.whatsappclone.services;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MyFirebaseMessagingClient extends FirebaseMessagingService {
+
+    //Servira para enviar token de notificaciones de dispositivo a dispositivo
+    @Override
+    public void onNewToken(@NonNull @NotNull String s) {
+        super.onNewToken(s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull @NotNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
