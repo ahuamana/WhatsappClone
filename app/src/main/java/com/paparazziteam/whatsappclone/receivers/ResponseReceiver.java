6bf419ff395d84cf19ca5f3ba219f00c91ac6a28
Alikhan <73962071@continental.edu.pe>
2021-08-05 13:57:49 -0500

    capture text of action on response notification

--- app/src/main/java/com/paparazziteam/whatsappclone/receivers/ResponseReceiver.java
+++ app/src/main/java/com/paparazziteam/whatsappclone/receivers/ResponseReceiver.java
@@ -3,10 +3,29 @@ package com.paparazziteam.whatsappclone.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.core.app.RemoteInput;
+
+import static com.paparazziteam.whatsappclone.services.MyFirebaseMessagingClient.NOTIFICATION_REPLY;
 
 public class ResponseReceiver  extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        String message = getMessageText(intent).toString();
+        android.util.Log.e("NOTIFICATION: ","Mensaje input: "+message);
+    }
+
+    private CharSequence getMessageText(Intent intent)
+    {
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+
+        if(remoteInput != null)
+        {
+            return remoteInput.getCharSequence(NOTIFICATION_REPLY);
+        }
+
+        return null;
     }
 }
